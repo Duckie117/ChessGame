7e26baabfba9a6855a60ee188c60b888a8b1fde3
Duckie117 <ngotheduc117@gmail.com>
2024-07-04 20:31:10 +0700

    Commit cho vui de up file thoi

---
+++ src/com/chess/engine/player/Player.java
@@ -0,0 +1,70 @@
+package com.chess.engine.player;
+
+import com.chess.engine.Alliance;
+import com.chess.engine.board.Board;
+import com.chess.engine.board.Move;
+import com.chess.engine.pieces.King;
+import com.chess.engine.pieces.Piece;
+
+import java.util.Collection;
+
+public abstract class Player {
+
+    protected final Board board;
+    protected final King playerKing;
+    protected final Collection<Move> legalMoves;
+
+    Player(final Board board, final Collection<Move> legalMoves, final Collection<Move> opponentMoves) {
+        this.board = board;
+        this.playerKing = establishKing();
+        this.legalMoves = legalMoves;
+    }
+
+    private King establishKing() {
+        for(final Piece piece : getActivePieces()) {
+            if(piece.getPieceType().isKing()) {
+                return (King) piece;
+            }
+        }
+        throw new RuntimeException("Should not reach here! Not a valid board!");
+    }
+
+    public boolean isMoveLegal(final Move move) {
+        return this.legalMoves.contains(move);
+    }
+
+    public boolean isInCheck() {
+        return this.board.getCurrentPlayer().getOpponent().getLegalMoves().stream()
+                .anyMatch(move -> move.getDestinationCoordinate() == this.playerKing.getPiecePosition());
+    }
+
+    public boolean isInCheckMate() {
+        return this.isInCheck() && !hasEscapeMoves();
+    }
+
+    public boolean isInStaleMate() {
+        return !this.isInCheck() && !hasEscapeMoves();
+    }
+
+    public boolean isCastled() {
+        return false;
+    }
+
+    public MoveTransition makeMove(final Move move) {
+        if(!isMoveLegal(move)) {
+            return new MoveTransition(this.board, move, MoveStatus.ILLEGAL_MOVE);
+        }
+        final Board transitionBoard = move.execute();
+        final Collection<Move> kingAttacks = Player.calculateAttacksOnTile(transitionBoard.getCurrentPlayer().getOpponent().getPlayerKing().getPiecePosition(),
+                transitionBoard.getCurrentPlayer().getLegalMoves());
+        if(!kingAttacks.isEmpty()) {
+            return new MoveTransition(this.board, move, MoveStatus.LEAVES_PLAYER_IN_CHECK);
+        }
+        return new MoveTransition(transitionBoard, move, MoveStatus.DONE);
+    }
+
+    public abstract Collection<Piece> getActivePieces();
+    public abstract Alliance getAlliance();
+    public abstract Player getOpponent();
+
+}
